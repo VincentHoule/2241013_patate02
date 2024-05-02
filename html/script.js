b88dcc635342407131dd34656587744904e23c0d
VincentHoule <2241013@carrefour.cegepvicto.ca>
2024-05-02 15:06:37 -0400

    enfin fini 2024-05-02

--- html/script.js
+++ html/script.js
@@ -4,22 +4,28 @@ async function nouvelleCle() {
     let cle = document.getElementById("cle");
 
     let utilisateur = {
-        courriel: `"${courriel01}"`,
-        password: `"${mdp01}"`
+        courriel: courriel01,
+        mot_de_passe: mdp01
     }
 
     let url = "https://two241013-patate01.onrender.com";
 
     const requete = await fetch(`${url}/tachesMemoire/utilisateurs/cle`, {
-        method: 'GET',
+        method: 'POST',
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify(utilisateur)
     })
-        .then(reponse => reponse.json())
-        .then(data => {
-            cle.innerHTML = data[0].cle;
+    let reponse = await requete.json()
 
-        })
-
+    if (requete.ok) {
+        cle.innerHTML = `Clé api : ${reponse.cle}`
+    }
+    else {
+        cle.innerHTML = `Clé api : ${reponse.message}`
+    }
 }
 
 async function nouveauUtilisateur() {
@@ -27,28 +33,32 @@ async function nouveauUtilisateur() {
     let prenom = document.getElementById("prenom").value;
     let courriel02 = document.getElementById("courriel02").value;
     let mdp02 = document.getElementById("mdp02").value;
-    let message = document.getElementById("message2")
-    let cle2 = document.getElementById("cle2");
+    let message = document.getElementById("message2");
+    let cle2 = document.getElementById("clee");
 
     let utilisateur = {
-        nom: `${nom}`,
-        prenom: `${prenom}`,
-        courriel: `${courriel02}`,
-        password: `${mdp02}`
+        nom: nom,
+        prenom: prenom,
+        courriel: courriel02,
+        mot_de_passe: mdp02
     }
-
     let url = "https://two241013-patate01.onrender.com";
     console.log(utilisateur);
-    let requete = await fetch(`https://two241013-patate01.onrender.com/tachesMemoire/utilisateurs/`, {
+    let requete = await fetch(`${url}/tachesMemoire/utilisateurs/`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-          },
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
         body: JSON.stringify(utilisateur)
-    });
-    console.log(requete)
-    message.innerHTML = requete.Message;
-    cle2.innerHTML = requete.cle_api;
-
+    })
+    let reponse = await requete.json()
+    if (requete.ok) {
+        message.innerHTML = `Message : ${reponse.message}`;
+        cle2.innerHTML = `Clé d'api : ${reponse.cle_api}`;
+    }
+    else {
+        message.innerHTML = `Message : ${reponse.message}`;
+    }
 
 }
